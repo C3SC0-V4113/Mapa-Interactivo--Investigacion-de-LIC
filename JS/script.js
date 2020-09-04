5a41ac93cb620fa5cb8dccff2934f79cb1cd220e
Daguid <62686204+David5034@users.noreply.github.com>
2020-09-04 14:55:36 -0600

    Creacion de mode-dark/light
    Para eso necesite el uso de JS por tanto se adjuntará el archivo nuevo de JS

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+function switchSheet() {
+    let theme = document.getElementById("theme");
+  
+    if (theme.getAttribute("href") == "css/light-mode.css") {
+      theme.href = "css/dark-mode.css";
+    } else {
+      theme.href = "css/light-mode.css";
+    }
+  }
