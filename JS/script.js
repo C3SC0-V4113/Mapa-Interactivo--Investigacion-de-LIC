5be17cbac7f81e26fc6acb2e888c15736ddafadb
C3SC0-V4113 <C3SC0-V4113@users.noreply.github.com>
2020-09-20 14:41:28 -0600

    Diseño nuevo para las paginas de departamentos

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,21 @@ function switchSheet() {
     let theme = document.getElementById("theme");
   
     if (theme.getAttribute("href") == "css/light-mode.css") {
+      console.log('modo oscuro')
       theme.href = "css/dark-mode.css";
     } else {
+      console.log('modo claro')
       theme.href = "css/light-mode.css";
+    }
+  }
+  function switchSheet1() {
+    let theme = document.getElementById("theme");
+  
+    if (theme.getAttribute("href") == "../css/light-mode.css") {
+      console.log('modo oscuro')
+      theme.href = "../css/dark-mode.css";
+    } else {
+      console.log('modo claro')
+      theme.href = "../css/light-mode.css";
     }
   }
