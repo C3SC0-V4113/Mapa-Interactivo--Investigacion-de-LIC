31769d6874e7b7119c47ebb03a6beb166e882b52
kathemeaw <70289118+kathemeaw@users.noreply.github.com>
2020-09-21 23:01:21 -0600

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -20,3 +20,23 @@ function switchSheet() {
       theme.href = "../css/light-mode.css";
     }
   }
+
+  // Obteniendo Posiciones
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption"); 
+
+//Esta funcion hace uso de su parametro para obtener toda la informacion de la imagen
+function ImagenModal(Funcion) {
+  modal.style.display = "block";
+  modalImg.src = Funcion.src;
+  captionText.innerHTML = Funcion.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
